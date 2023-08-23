9b489449c4f44ada52af2e73026a8d218dbcebc9
vuthin <thiravuthin.thoeun@cubetiqs.com>
2023-08-23 18:34:00 +0700

    vuthin

--- src/main/java/com/kosign/school_management/payload/student/StudentRequest.java
+++ src/main/java/com/kosign/school_management/payload/student/StudentRequest.java
@@ -1,14 +1,10 @@
 package com.kosign.school_management.payload.student;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Date;
